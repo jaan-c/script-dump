1b65a7c0a220cec32b6254d22b3b21692bf1f4e8
Jaan <jaan-paul@users.noreply.github.com>
2020-05-31 17:02:16 +0800

    Simplify image dithering implementation.

--- fs-dither/dither_image.py
+++ fs-dither/dither_image.py
@@ -1,51 +1,41 @@
 from typing import *
+from skimage.color import rgb2gray
 import numpy as np
 
 
-def dither_image(image: np.ndarray, *, shade_count=4) -> np.ndarray:
-    """ Dither all channels using dither_func. """
+def dither_image(
+    image: np.ndarray, *, grayscale=True, shade_count=4
+) -> np.ndarray:
+    """ Dither image with Floyd-Steinberg algorithm. """
 
-    if shade_count < 1:
-        raise ValueError(f"shade_count must be >= 2, got {shade_count}.")
+    image = rgb2gray(image) if grayscale else image
 
-    if _is_2d(image):
-        return _dither_channel(image, shade_count=shade_count)
-    elif _has_channel_count(image, 3):
-        return _dither_rgb(image, shade_count=shade_count)
-    elif _has_channel_count(image, 4):
-        return _dither_rgba(image, shade_count=shade_count)
+    if len(image.shape) == 2:
+        return _dither_channel(image, shade_count)
+
+    elif len(image.shape) == 3 and image.shape[2] in (3, 4):
+        dithered_color_channels: List[np.ndarray] = []
+        for color_channel_ix in range(3):
+            color_channel = image[:, :, color_channel_ix]
+            dithered = _dither_channel(color_channel, shade_count)
+
+            dithered_color_channels.append(dithered)
+
+        channels = [*dithered_color_channels]
+        if image.shape[2] == 4:  # If has alpha channel.
+            channels.append(image[:, :, 3])
+
+        return np.dstack(channels)
+
     else:
-        raise ValueError(f"Unhandled image shape {image.shape}.")
-
-
-def _dither_rgba(image: np.ndarray, shade_count: int) -> np.ndarray:
-    if not _has_channel_count(image, 4):
-        raise ValueError("image must have exactly 4 channels.")
-
-    dithered_rgb = _dither_rgb(image[:, :, :3])
-    return np.vstack((dithered_rgb, image[:, :, 3]))
-
-
-def _dither_rgb(image: np.ndarray, shade_count: int) -> np.ndarray:
-    """ Dither an image with RGB channels. """
-
-    if not _has_channel_count(image, 3):
-        raise ValueError("image must have exactly 3 color channels.")
-
-    dithered_channels: List[np.ndarray] = []
-    for channel_ix in range(3):
-        channel = image[:, :, channel_ix]
-        dithered = _dither_channel(channel, shade_count)
-
-        dithered_channels.append(dithered)
-
-    return np.array(dithered_channels)
+        raise ValueError(f"Invalid image shape {image.shape}.")
 
 
 def _dither_channel(channel: np.ndarray, shade_count: int) -> np.ndarray:
-    """ Dither a single channel of image using Floyd-Steinberg algorithm. """
+    """ Dither a single channel (2 dimensional array) of image using 
+        Floyd-Steinberg algorithm. """
 
-    if not _is_2d(channel):
+    if len(channel.shape) != 2:
         raise ValueError(f"channel must exactly have 2 dimensions.")
     elif shade_count < 2:
         raise ValueError("shade_count must be minimum of 2.")
@@ -76,29 +66,3 @@ def _dither_channel(channel: np.ndarray, shade_count: int) -> np.ndarray:
                     pass
 
     return copy
-
-
-def _is_2d(ndarray: np.ndarray) -> bool:
-    return len(ndarray.shape) == 2
-
-
-def _is_3d(ndarray: np.ndarray) -> bool:
-    return len(ndarray.shape) == 3
-
-
-def _has_channel_count(image: np.ndarray, expected_count: int) -> bool:
-    if not _is_3d(image):
-        raise ValueError("image must have exactly 3 dimensions.")
-    else:
-        return image.shape[2] == expected_count
-
-
-def _are_values_in_range(
-    ndarray: np.ndarray, minimum: float, maximum: float
-) -> bool:
-    """ Check if values in ndarray are in inclusive [minimum, maximum] range. """
-
-    smallest = ndarray.min()
-    biggest = ndarray.max()
-
-    return minimum <= smallest <= maximum and minimum <= biggest <= maximum
