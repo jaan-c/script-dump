2e7f5a113d855dcf41f05f65bc0a62a06fa2d3b1
jaan-c <jaan-c@users.noreply.github.com>
2020-08-30 01:01:32 +0800

    Initial cli.py.

---
+++ deduplicate/cli.py
@@ -0,0 +1,30 @@
+from typing import *
+from duplicates import FileInfo
+
+
+class Cli:
+    duplicates: Mapping[str, List[FileInfo]]
+
+    def __init__(self, duplicates: Mapping[str, List[FileInfo]]):
+        self.duplicates = duplicates
+
+    def run(self) -> None:
+        raise NotImplementedError()
+
+
+class InteractiveCli(Cli):
+    def run(self) -> None:
+        pass
+
+
+class NonInteractiveCli(Cli):
+    keep_most_recent: bool
+
+    def __init__(
+        self, duplicates: Mapping[str, List[FileInfo]], keep_most_recent: bool
+    ):
+        super(NonInteractiveCli, self).__init__(duplicates)
+        self.keep_most_recent = keep_most_recent
+
+    def run(self) -> None:
+        pass
