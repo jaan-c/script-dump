dcaddb7a25dde5c05169debbff652550f5398623
jaan-c <jaan-c@users.noreply.github.com>
2020-08-29 20:06:48 +0800

    Redo duplicate finding code.

--- deduplicate/duplicates.py
+++ deduplicate/duplicates.py
@@ -6,34 +6,33 @@ K = TypeVar("K")
 V = TypeVar("V")
 
 
-def find_duplicates(file_infos: Iterable[FileInfo]) -> Mapping[str, List[str]]:
+def find_duplicates(
+    file_infos: Iterable[FileInfo],
+) -> Mapping[str, List[FileInfo]]:
     """ Find duplicates in infos and return it as a mapping of hash and file 
         paths. This skips over zero-byte files.
     """
 
-    file_infos = filter(lambda i: i.get_size() > 0, file_infos)
+    size_group = _group_by_key(file_infos, lambda i: i.get_size())
+    size_group = {
+        size: infos for size, infos in size_group.items() if size != 0
+    }
+    size_group = _remove_singleton_values(size_group)
+    infos = _ungroup(size_group)
 
-    partial_hash_group = _group_by_key(
-        file_infos, lambda i: i.get_partial_hash()
-    )
+    partial_hash_group = _group_by_key(infos, lambda i: i.get_partial_hash())
     partial_hash_group = _remove_singleton_values(partial_hash_group)
+    infos = _ungroup(partial_hash_group)
 
-    hash_group: Dict[str, List[FileInfo]] = {}
-    for infos in partial_hash_group.values():
-        group = _group_by_key(infos, lambda i: i.get_hash())
-        hash_group.update(group)
+    hash_group = _group_by_key(infos, lambda i: i.get_hash())
+    hash_group = _remove_singleton_values(hash_group)
 
-    return _remove_singleton_values(
-        {
-            hash: list(map(lambda i: i.path, infos))
-            for hash, infos in hash_group.items()
-        }
-    )
+    return hash_group
 
 
 def _group_by_key(
     values: Iterable[V], derive_group_key: Callable[[V], K]
-) -> Dict[K, List[V]]:
+) -> Mapping[K, List[V]]:
     """ Group values by a key derived from each value. """
 
     group: Dict[K, List[V]] = {}
@@ -46,12 +45,15 @@ def _group_by_key(
 
     return group
 
+
 def _ungroup(group: Mapping[K, Iterable[V]]) -> Iterable[V]:
     """ Remove grouping by merging all iterable values into one single iterable. 
     """
 
-    return itertools.chain.from_iterable(group.values)
+    return itertools.chain.from_iterable(group.values())
 
 
-def _remove_singleton_values(mapping: Mapping[K, List[V]]) -> Mapping[K, List[V]]:
+def _remove_singleton_values(
+    mapping: Mapping[K, List[V]]
+) -> Mapping[K, List[V]]:
     return {k: v for k, v in mapping.items() if len(v) != 1}
