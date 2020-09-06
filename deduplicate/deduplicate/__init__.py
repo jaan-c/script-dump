4f3cd65179d76a98c8fa05593af6e5afd48a81ea
jaan-c <jaan-c@users.noreply.github.com>
2020-09-06 19:33:49 +0800

    Don't handle direct __init__.py execution.
    python's -m argument calls __main__.py not __init__.py.

--- deduplicate/deduplicate/__init__.py
+++ deduplicate/deduplicate/__init__.py
@@ -1,9 +1,3 @@
-if __name__ == "__main__":
-    import sys
-    from . import cli
-
-    cli.execute(sys.argv)
-else:
-    from .duplicate import find_duplicates
-    from .delete import delete_duplicates
-    from . import keep, fileinfo
+from .duplicate import find_duplicates
+from .delete import delete_duplicates
+from . import keep, fileinfo
