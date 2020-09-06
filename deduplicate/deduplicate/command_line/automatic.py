7abe36d5ff46cfd3cac1961f0ea3a26cf04535ae
jaan-c <jaan-c@users.noreply.github.com>
2020-09-06 19:56:04 +0800

    Log deletions in automatic_delete.

--- deduplicate/deduplicate/command_line/automatic.py
+++ deduplicate/deduplicate/command_line/automatic.py
@@ -1,4 +1,5 @@
 from typing import *
+import os
 from .. import delete_duplicates, keep
 
 
@@ -7,4 +8,11 @@ def automatic_delete(
     keep_filter: Literal["last_modified"],
 ):
     for duplicates in duplicate_groups.values():
-        delete_duplicates(keep.last_modified(), duplicates)
+        delete_duplicates(
+            keep.last_modified(), duplicates, delete_file=_delete_with_logging
+        )
+
+
+def _delete_with_logging(file_path: str) -> None:
+    os.remove(file_path)
+    print(f"deleted {file_path}")
