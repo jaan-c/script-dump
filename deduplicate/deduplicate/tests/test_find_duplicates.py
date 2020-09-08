0dd14191d31905f93b4b97ee7cae7368916d8fd2
jaan-c <jaan-c@users.noreply.github.com>
2020-09-08 12:58:37 +0800

    Use positional form of argument.

--- deduplicate/deduplicate/tests/test_find_duplicates.py
+++ deduplicate/deduplicate/tests/test_find_duplicates.py
@@ -10,21 +10,21 @@ def test_find_duplicates():
 
     with tempfile.TemporaryDirectory() as dir_path:
         with util.temp_file_with_rand_content(
-            0, dir_path=dir_path
+            0, dir_path
         ) as z1, util.temp_file_with_rand_content(
-            0, dir_path=dir_path
+            0, dir_path
         ) as z2, util.temp_file_with_content(
-            head1 + body, dir_path=dir_path
+            head1 + body, dir_path
         ) as ph1, util.temp_file_with_content(
-            head2 + body, dir_path=dir_path
+            head2 + body, dir_path
         ) as ph2, util.temp_file_with_content(
-            head3 + body, dir_path=dir_path
+            head3 + body, dir_path
         ) as h1, util.temp_file_with_content(
-            head3 + body, dir_path=dir_path
+            head3 + body, dir_path
         ) as h2, util.temp_file_with_rand_content(
-            2048, dir_path=dir_path
+            2048, dir_path
         ) as r1, util.temp_file_with_rand_content(
-            2048, dir_path=dir_path
+            2048, dir_path
         ) as r2:
             duplicate_groups = find_duplicates(dir_path)
 
