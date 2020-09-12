d853c8774a0bd6efcced9f048d29d116fb04e982
jaan-c <jaan-c@users.noreply.github.com>
2020-09-12 22:04:14 +0800

    Added setup.py.

---
+++ ftag/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="ftag",
+    version="0.5",
+    description="Tag files for easy searching.",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: System :: Filesystems",
+    ],
+    author="jaan-c",
+    license="MIT",
+    entry_points={"console_scripts": ["ftag=ftag.__main__:main"]},
+    packages=["ftag"],
+    install_requires=[],
+    zip_safe=True,
+    include_package_data=False,
+)
