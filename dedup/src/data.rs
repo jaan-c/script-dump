7f74cfc9636a203750452a85146fa71541a94a52
jaan-c <jaan-c@users.noreply.github.com>
2021-02-06 17:02:28 +0800

    Added tests.

--- dedup/src/data.rs
+++ dedup/src/data.rs
@@ -1,4 +1,3 @@
-use core::panic;
 use std::path::PathBuf;
 
 #[derive(Debug)]
@@ -17,5 +16,38 @@ impl Duplicate {
             hash: hash,
             files: files,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use super::Duplicate;
+
+    #[test]
+    fn duplicate_new() {
+        let dup = Duplicate::new(
+            "Hello".to_string(),
+            vec![
+                Path::new("Hello").to_path_buf(),
+                Path::new("World").to_path_buf(),
+            ],
+        );
+
+        assert_eq!(dup.hash, "Hello");
+        assert_eq!(
+            dup.files,
+            vec![
+                Path::new("Hello").to_path_buf(),
+                Path::new("World").to_path_buf(),
+            ]
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn duplicate_new_invalid_files() {
+        Duplicate::new("Hello".to_string(), vec![Path::new("Hello").to_path_buf()]);
     }
 }
